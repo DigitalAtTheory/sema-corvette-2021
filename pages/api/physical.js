c4f185e239655a651eddb89e2ef358e30884c0de
Josh Suson <josh.suson@gmail.com>
2021-10-26 16:03:41 -0400

    Trying to fix form posting

--- pages/api/physical.js
+++ pages/api/physical.js
@@ -1,4 +1,5 @@
 import Airtable from "airtable";
+import axios from "axios";
 
 module.exports = async (req, res) => {
   const base = new Airtable({ apiKey: process.env.AIRTABLE_API_KEY }).base(
@@ -20,8 +21,39 @@ module.exports = async (req, res) => {
     personal_car,
   } = req.body;
 
-  base("Physical").create(
-    [
+  // base("Physical").create(
+  //   [
+  //     {
+  //       fields: {
+  //         entry_id,
+  //         gender,
+  //         age: parseInt(age),
+  //         email,
+  //         zip_code: parseInt(zip_code),
+  //         privacy_policy,
+  //         manufacturer,
+  //         hoonigan,
+  //         luftgekühlt,
+  //         race_service,
+  //         oil_change,
+  //         personal_car,
+  //       },
+  //     },
+  //   ],
+  //   function (err, records) {
+  //     if (err) {
+  //       console.error(err);
+  //       return;
+  //     }
+  //     records.forEach(function (record) {
+  //       console.log(record.getId());
+  //     });
+  //   }
+  // );
+
+  await axios
+    .post(
+      `https://api.airtable.com/v0/${process.env.AIRTABLE_BASE}/Physical`,
       {
         fields: {
           entry_id,
@@ -38,17 +70,15 @@ module.exports = async (req, res) => {
           personal_car,
         },
       },
-    ],
-    function (err, records) {
-      if (err) {
-        console.error(err);
-        return;
+      {
+        headers: {
+          Authorization: `Bearer ${process.env.AIRTABLE_API_KEY}`,
+          "Content-Type": "application/json",
+        },
       }
-      records.forEach(function (record) {
-        console.log(record.getId());
-      });
-    }
-  );
+    )
+    .then((data) => console.log(data))
+    .catch((err) => console.error(err));
 
   res.status(200).send("Logged");
 };
