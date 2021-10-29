b2b27c6c30354cf20a32e4384b9b96cb99df26e1
Josh Suson <josh.suson@gmail.com>
2021-10-29 10:50:50 -0400

    Working on wallpaper photos

--- components/WallpaperDownload.js
+++ components/WallpaperDownload.js
@@ -1,17 +1,13 @@
 import Image from "next/image";
 import Link from "next/link";
+import phoneMockup from "../public/wallpaper-phone-corvette.png";
 
 export default function WallpaperDownload({ wallpaperLink }) {
   return (
     <div className="text-center text-white px-4">
       <hr className="text-white my-16 w-1/2 mx-auto" />
       <h2 className="text-white mb-6 text-4xl">Download the Wallpaper</h2>
-      <Image
-        src="/wallpaper-phone-corvette.png"
-        width="1170px"
-        height="1310px"
-        priority
-      />
+      <Image src={phoneMockup} placeholder="blur" priority />
       <Link href={wallpaperLink}>
         <a className="bg-reflex-600 text-white w-3/4 lg:w-1/2 mx-auto mt-6 py-3 rounded shadow-xl block cursor-pointer">
           Click to Download
